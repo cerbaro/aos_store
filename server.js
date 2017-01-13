5579542a822bdba956c7a071e45bd792eb6818a9
Vinícius Andrei Cerbaro <contato@cerbaro.net>
2017-01-13 17:03:23 -0500

    Initial commit. Adding basic stuff..

---
+++ server.js
@@ -0,0 +1,21 @@
+var restify     = require('restify'),
+    passport    = require('passport'),
+    mongoose    = require('mongoose');
+
+
+var app = restify.createServer({
+    name: "AOS Store",
+    formatters: {
+        'application/json': function(req, res, body, callback) {
+            return callback(null, JSON.stringify(body, null, space=2));
+        }
+    }
+});
+
+
+
+app.listen(8000);
+
+console.log(" ");
+console.log(app.name, "listening on port", 8000);
+console.log(" ");
